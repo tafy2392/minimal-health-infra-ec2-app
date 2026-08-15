50db82b6644454775c2554295c64ec42af69c9e7
QXZ2ZRU <Munyaradzi.Tafirenyika@partner.bmw.co.za>
2026-08-15 21:01:59 +0200

    Setup s3 backend for tf, allow dynamodb lock

---
+++ terraform/health-dev-aws-eu/remote-state/main.tf
@@ -0,0 +1,14 @@
+module "backend" {
+  source                       = "squareops/tfstate/aws"
+  version                      = "1.1.1"
+  logging                      = true
+  bucket_name                  = "dev-health-aws-tfstate-bucket" #unique global s3 bucket name
+  environment                  = "dev"
+  force_destroy                = true
+  versioning_enabled           = true
+  cloudwatch_logging_enabled   = true
+  log_retention_in_days        = 90
+  log_bucket_lifecycle_enabled = true
+  s3_ia_retention_in_days      = 90
+  s3_galcier_retention_in_days = 180
+}
