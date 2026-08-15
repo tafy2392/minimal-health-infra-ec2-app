300918b5668a7054e858766c9f95216ae6408d27
QXZ2ZRU <Munyaradzi.Tafirenyika@partner.bmw.co.za>
2026-08-15 23:03:31 +0200

    Setup igw resource

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,18 +1,18 @@
 locals {
 
   new_bits = 26 - split("/", var.cidr_block)[1]
-  
+
   # Calculate total number of possible subnets (2^new_bits, e.g., 2^2 = 4)
   total_subnets = pow(2, local.new_bits)
-  
+
   # Generate all subnet CIDRs dynamically using a loop
   all_cidrs = [
     for i in range(local.total_subnets) : cidrsubnet(var.cidr_block, local.new_bits, i)
   ]
-  
+
   # Split the list cleanly in half using slice()
   # First 2 become public, last 2 become private
-  half_count     = local.total_subnets / 2
+  half_count    = local.total_subnets / 2
   public_cidrs  = slice(local.all_cidrs, 0, local.half_count)
   private_cidrs = slice(local.all_cidrs, local.half_count, local.total_subnets)
 }
