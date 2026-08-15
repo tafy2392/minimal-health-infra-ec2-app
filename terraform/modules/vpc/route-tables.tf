0530fe113aff2aecdd68f76e7bd2ccd8da498a58
QXZ2ZRU <Munyaradzi.Tafirenyika@partner.bmw.co.za>
2026-08-15 23:55:42 +0200

    Add route table association

--- terraform/modules/vpc/route-tables.tf
+++ terraform/modules/vpc/route-tables.tf
@@ -15,3 +15,17 @@ resource "aws_route_table" "public" {
     Name = "${var.environment}-public"
   }
 }
+
+resource "aws_route_table_association" "private" {
+  for_each = { for idx, cidr in local.private_cidrs : idx => cidr }
+
+  subnet_id      = aws_subnet.private[each.key].id
+  route_table_id = aws_route_table.private[each.key].id
+}
+
+resource "aws_route_table_association" "public" {
+  for_each = { for idx, cidr in local.public_cidrs : idx => cidr }
+   
+  subnet_id      = aws_subnet.public[each.key].id
+  route_table_id = aws_route_table.public.id
+}
