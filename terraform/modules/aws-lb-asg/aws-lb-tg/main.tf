527e24f1e64d4e3e6be8d59e168be0847d30e4c7
QXZ2ZRU <Munyaradzi.Tafirenyika@partner.bmw.co.za>
2026-08-16 18:00:13 +0200

    Add health path for nginx proxy

--- terraform/modules/aws-lb-asg/aws-lb-tg/main.tf
+++ terraform/modules/aws-lb-asg/aws-lb-tg/main.tf
@@ -108,6 +108,18 @@ resource "aws_lb_target_group" "app" {
   vpc_id      = var.vpc_id
   target_type = "instance"
 
+  health_check {
+    enabled             = true
+    path                = "/healthz"
+    port                = "traffic-port"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+  }
+
 }
 
 resource "aws_autoscaling_policy" "scale_on_memory" {
